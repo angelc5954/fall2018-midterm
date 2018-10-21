898f32ff9bb45f4885af1cc52ed56720255e466c
Angel Castaneda <angelcastaneda@cis.vvc.edu>
2018-10-20 21:33:26 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,14 +1,51 @@
 /**
 Description:
-Filename:
-Author:
-Date:
-*/
+Filename: main.cpp
+Author: Angel Castaneda
+Date: 10/18/18
+**/
+
+#include<iostream>
+#include<cstring>
 
 using namespace std;
 
+string get_string(string prompt);
+int char_count(string,char);
 
 int main()
 {
-   return 0;
+    string str = get_string("Enter a string: ");
+
+    char chr = 'a'; 
+
+    cout << "A shows up " << char_count(str,chr) << " times." << endl;
+    return 0;
+}
+
+string get_string(string prompt)
+{
+    string input;
+    cout << prompt;
+    cin >> input;
+    return (input);
+}
+
+int char_count(string s,char c)
+{
+    int count = 0;
+ 
+    for(int i = 0; i < s.size(); i++)
+    { 
+        if(s.at(i) == c)
+        {    
+            count++;
+        }
+        else
+        {
+            count == count;
+        }
+    } 
+
+    return (count);
 }
